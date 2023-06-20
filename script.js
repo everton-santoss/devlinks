f56da1c977e4bdb3a6ac8e1f92757ec5dcf2394a
Everton Santos <evertonsantos0707@gmail.com>
2023-06-20 20:01:18 -0300

    Go

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 function toggleMode() {
-    const html = document.documentElement
+  const html = document.documentElement
 
-    if(html.classList.contains('light')) {
-        html.classList.remove('light')
-    } else {
-        html.classList.add('light')
-    }
+  if (html.classList.contains("light")) {
+    html.classList.remove("light")
+  } else {
+    html.classList.add("light")
+  }
 
-    //html.classList.toggle('light')
+  //html.classList.toggle('light')
 }
